d6ada533abf120b382ff80c3f757536270bbd3c9
Heiko Stuebner <heiko@sntech.de>
2014-06-20 00:23:56 +0200

    disable the cpufreq-cpu0 driver while we meddle with the clocks

--- arch/arm/mach-rockchip/rockchip.c
+++ arch/arm/mach-rockchip/rockchip.c
@@ -93,7 +93,7 @@ static void __init rockchip_dt_init(void)
 //	l2x0_of_init(0, ~0UL);
 
 	of_platform_populate(NULL, of_default_bus_match_table, NULL, NULL);
-	platform_device_register_full(&devinfo);
+//	platform_device_register_full(&devinfo);
 }
 
 static const char * const rockchip_board_dt_compat[] = {
