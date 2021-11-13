bbde4844540056a1cec7ed1befe6b1eb874aa019
Heiko Stuebner <heiko@sntech.de>
2021-11-13 21:21:40 +0100

    Revert "RISC-V: Add arch functions for non-retentive suspend entry/exit"
    This reverts commit 423c826a9dd7a5e73aa002ce863d7f456e30e9f3.

--- arch/riscv/kernel/asm-offsets.c
+++ arch/riscv/kernel/asm-offsets.c
@@ -10,7 +10,6 @@
 #include <linux/sched.h>
 #include <asm/thread_info.h>
 #include <asm/ptrace.h>
-#include <asm/suspend.h>
 
 void asm_offsets(void);
 
@@ -111,8 +110,6 @@ void asm_offsets(void)
 	OFFSET(PT_STATUS, pt_regs, status);
 	OFFSET(PT_BADADDR, pt_regs, badaddr);
 	OFFSET(PT_CAUSE, pt_regs, cause);
-
-	OFFSET(SUSPEND_CONTEXT_REGS, suspend_context, regs);
 
 	/*
 	 * THREAD_{F,X}* might be larger than a S-type offset can handle, but
